02366fc877d50992f33bfe9e22686ae4e9f12c0b
Daniel Rubio <drubio1989@gmail.com>
2014-03-01 18:35:07 -0500

    Added noncore component (positions)

--- server.rb
+++ server.rb
@@ -1,6 +1,5 @@
 require 'sinatra'
 require 'csv'
-require 'pry'
 
 before do
 	@roster_info = []
@@ -9,14 +8,7 @@ before do
 	end
 end
 
-# CSV.foreach('lackp_starting_rosters.csv', headers: true) do |row|
-#     if row["team"].downcase == @heading
-#       @players << row.to_hash
-#     end
-#   end
-
 get '/:team' do
-	#binding.pry
 	@team_players = []
 	@specific_team = params[:team].gsub!(/_/," ") #Jetson Jets
   @roster_info.each do |item|
@@ -25,6 +17,17 @@ get '/:team' do
   	end
   end
   erb :index
+end
+
+get '/:position' do
+	@player_positions = []
+	@specific_position = params[:position].gsub!(/_/," ")
+  @roster_info.each do |item|
+  	if item['position'] == @specific_position
+  		@player_positions << item
+  	end
+  end
+  erb :positions
 end
 
 

311df948775aceca63f88bc63130aa4e5b8fe692
Eric Kelly <heroiceric@gmail.com>
2014-03-02 10:54:41 -0500

    Fix indentation
    You should use two spaces for indentation.
    
    Make sure you have all of these settings set up for Sublime Text
    https://gist.github.com/HeroicEric/77c860d2fe8d7ae0a98f

--- server.rb
+++ server.rb
@@ -2,34 +2,30 @@ require 'sinatra'
 require 'csv'
 
 before do
-	@roster_info = []
-	CSV.foreach('lackp_starting_rosters.csv',headers:true) do |row|
-		@roster_info << row.to_hash
-	end
+  @roster_info = []
+  CSV.foreach('lackp_starting_rosters.csv',headers:true) do |row|
+    @roster_info << row.to_hash
+  end
 end
 
 get '/:team' do
-	@team_players = []
-	@specific_team = params[:team].gsub!(/_/," ") #Jetson Jets
+  @team_players = []
+  @specific_team = params[:team].gsub!(/_/," ") #Jetson Jets
   @roster_info.each do |item|
-  	if item['team'] == @specific_team
-  		@team_players << item
-  	end
+    if item['team'] == @specific_team
+      @team_players << item
+    end
   end
   erb :index
 end
 
 get '/:position' do
-	@player_positions = []
-	@specific_position = params[:position].gsub!(/_/," ")
+  @player_positions = []
+  @specific_position = params[:position].gsub!(/_/," ")
   @roster_info.each do |item|
-  	if item['position'] == @specific_position
-  		@player_positions << item
-  	end
+    if item['position'] == @specific_position
+      @player_positions << item
+    end
   end
   erb :positions
 end
-
-
-
-
